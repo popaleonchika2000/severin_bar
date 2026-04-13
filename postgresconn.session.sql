155967f09f2fd7eea607a1ebcd709447a8ada1a4
popaleonchika2000 <sorokinan269@gmail.com>
2026-04-13 20:48:53 +0300

    update two

---
+++ postgresconn.session.sql
@@ -0,0 +1,6 @@
+INSERT INTO categories (name, name_ru, icon, sort_order) VALUES
+('all', 'Все коктейли', '🍸',1)
+('classic', 'Классические', '🥃',2)
+('popular', 'Популярные', '⭐',3)
+('author', 'Авторские', '👨‍🍳',4)
+('seasonal', 'Сезонные', '🌸',5)
